202ad1d89d00139c8ef4695fe2ad64192a2a48fc
Ellis <nardi0302@gmail.com>
2023-03-25 22:12:40 +0900

    Feat: Implement sample test code
    - 각 테스트 툴들이 정상동작하는지 확인하기 위해 샘플 코드를 작성하였습니다
    - UI 테스트 / mockAPI / 상호작용 / E2E 테스트 모두 통과하였습니다
    - 각자 로컬 환경에서 테스트 이상 없는지 확인한 뒤 삭제하면 될 것 같습니다

---
+++ src/components/common/Button/ButtonList.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { useTasks } from './useTasks';
+import { Button } from './Button';
+
+export interface ButtonList {
+  error: boolean;
+}
+
+export const tasks = {
+  tasks: [
+    { id: '1', state: 'TASK_INBOX', title: 'Build a date picker' },
+    { id: '2', state: 'TASK_INBOX', title: 'QA dropdown' },
+    {
+      id: '3',
+      state: 'TASK_INBOX',
+      title: 'Write a schema for account avatar component',
+    },
+    { id: '4', state: 'TASK_INBOX', title: 'Export logo' },
+    { id: '5', state: 'TASK_INBOX', title: 'Fix bug in input error state' },
+    { id: '6', state: 'TASK_INBOX', title: 'Draft monthly blog to customers' },
+  ],
+};
+
+export const ButtonList = ({ error = false }: ButtonList) => {
+  const [tasks, dispatch] = useTasks();
+
+  if (error) {
+    return (
+      <div style={{ width: '500px', height: '500px', backgroundColor: 'tomato' }}>Error!!!</div>
+    );
+  }
+  return (
+    <div title="tasks" style={{ width: '500px', height: '500px', backgroundColor: 'tomato' }}>
+      {tasks &&
+        tasks.map((task) => (
+          <Button width={100} height={100}>
+            {task.title}
+          </Button>
+        ))}
+    </div>
+  );
+};
