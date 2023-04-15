b01ee1fa663ef46ddf5f5e108e61503634be25a9
GuRaBang <qkrdbsgk00@gmail.com>
2023-04-16 03:00:36 +0900

    feat: set GlobalStyle and NormalizeStyle
    - font 추가
    - storybook 설정 추가

--- .storybook/decorators.jsx
+++ .storybook/decorators.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { mswDecorator } from 'msw-storybook-addon';
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { GlobalStyle } from '../src/styles/globalStyle';
+import { NormalizeStyle } from '../src/styles/normalizeStyle';
 
 const queryClient = new QueryClient();
 
@@ -9,6 +11,8 @@ export const globalDecorators = [
   (Story) => (
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
+        <NormalizeStyle />
+        <GlobalStyle />
         <Story />
       </QueryClientProvider>
     </RecoilRoot>
