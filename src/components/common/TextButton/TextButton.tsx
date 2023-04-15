86e8c96780e703e4e2b009eb30c65d812dd0c036
GuRaBang <qkrdbsgk00@gmail.com>
2023-04-16 03:07:08 +0900

    feat: add TextButton Component

---
+++ src/components/common/TextButton/TextButton.tsx
@@ -0,0 +1,64 @@
+import { MouseEventHandler } from 'react';
+import styled, { css } from 'styled-components';
+
+export interface ButtonProps {
+  type: 'button' | 'submit';
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  size: 'big' | 'midium' | 'small';
+  backgroundColor: string;
+  [key: string]: unknown;
+}
+
+export const TextButton = ({
+  type,
+  onClick,
+  size,
+  backgroundColor,
+  children,
+  ...props
+}: ButtonProps) => {
+  return (
+    <StyledButton
+      type={type}
+      onClick={onClick}
+      size={size}
+      backgroundColor={backgroundColor}
+      {...props}
+    >
+      {children}
+    </StyledButton>
+  );
+};
+
+const StyledButton = styled.button<ButtonProps>`
+  ${({ size }) => {
+    return BUTTON_SIZE[size];
+  }};
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  border: 1px solid #1d1d1d;
+  border-radius: 10px;
+`;
+
+const BUTTON_SIZE = {
+  big: css`
+    width: 80px;
+    height: 35px;
+    font-size: var(--text-bs);
+    font-weight: var(--text-bold);
+  `,
+  midium: css`
+    width: 68px;
+    height: 27px;
+    font-size: var(--text-sm);
+    font-weight: var(--text-regular);
+  `,
+  small: css`
+    width: 43px;
+    height: 27px;
+    font-size: var(--text-sm);
+    font-weight: var(--text-regular);
+  `,
+};
