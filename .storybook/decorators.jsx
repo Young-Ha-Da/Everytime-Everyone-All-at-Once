96332510b5ffab1aea20da515854e68bdb278e2a
Ellis <nardi0302@gmail.com>
2023-04-22 14:11:27 +0900

    Fix: Resolve build error

--- .storybook/decorators.jsx
+++ .storybook/decorators.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { mswDecorator } from 'msw-storybook-addon';
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { GlobalStyle, NormalizeStyle } from '@/styles';
+import { GlobalStyle } from './../src/styles';
 
 const queryClient = new QueryClient();
 
@@ -10,7 +10,6 @@ export const globalDecorators = [
   (Story) => (
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
-        <NormalizeStyle />
         <GlobalStyle />
         <Story />
       </QueryClientProvider>
--- cypress/e2e/auth.cy.ts
+++ cypress/e2e/auth.cy.ts
@@ -1,4 +1,19 @@
-import { tasks } from '@/components';
+// import { tasks } from '@/components';
+
+const tasks = {
+  tasks: [
+    { id: '1', state: 'TASK_INBOX', title: 'Build a date picker' },
+    { id: '2', state: 'TASK_INBOX', title: 'QA dropdown' },
+    {
+      id: '3',
+      state: 'TASK_INBOX',
+      title: 'Write a schema for account avatar component',
+    },
+    { id: '4', state: 'TASK_INBOX', title: 'Export logo' },
+    { id: '5', state: 'TASK_INBOX', title: 'Fix bug in input error state' },
+    { id: '6', state: 'TASK_INBOX', title: 'Draft monthly blog to customers' },
+  ],
+};
 
 describe('User Authenticate', () => {
   beforeEach(() => {
