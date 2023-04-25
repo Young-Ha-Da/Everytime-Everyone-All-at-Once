27b2f82fe3e5daff0773dc4821242262e5beadbf
Ellis <nardi0302@gmail.com>
2023-04-25 18:41:21 +0900

    Feat: Implement TimeRangeText component

---
+++ src/components/common/Text/TimeRangeText.tsx
@@ -0,0 +1,36 @@
+import styled, { FlattenSimpleInterpolation } from 'styled-components';
+
+export interface TimeRangeTextProps {
+  fromHour: string | number;
+  fromMinute: string | number;
+  toHour: string | number;
+  toMinute: string | number;
+  css: FlattenSimpleInterpolation;
+}
+
+export function TimeRangeText({ css, fromHour, fromMinute, toHour, toMinute }: TimeRangeTextProps) {
+  return (
+    <P css={css}>
+      <span>{fromHour}</span>
+      <span>:</span>
+      <span>{fromMinute}</span>
+      <span>~</span>
+      <span>{toHour}</span>
+      <span>:</span>
+      <span>{toMinute}</span>
+    </P>
+  );
+}
+
+const P = styled.p`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 236px;
+  height: 43px;
+  color: var(--black);
+  text-align: center;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+`;
