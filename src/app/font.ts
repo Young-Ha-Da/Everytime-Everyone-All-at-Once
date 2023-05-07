341b83f1c99e3b7e1416e40578dd56168945c98b
Ellis <nardi0302@gmail.com>
2023-05-07 11:15:43 +0900

    Chore: Remove unused var

--- src/app/font.ts
+++ src/app/font.ts
@@ -1,9 +1,5 @@
 import localFont from 'next/font/local';
 
-export const cafe24Font = localFont({
-  src: './../../public/assets/Cafe24Ssurround.woff2',
-  variable: '--font-cafe24',
-});
 export const utoFont = localFont({
   src: [
     {
